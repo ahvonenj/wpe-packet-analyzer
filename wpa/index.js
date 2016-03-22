04d1d4f0693dc0fff8300c1513401f6551738707
Jonah <jonah.ahvonen@gmail.com>
2016-03-22 19:12:03 +0200

    Finish structure mostly

--- wpa/index.js
+++ wpa/index.js
@@ -2,6 +2,40 @@ $(document).on('click', '#newpacket', function()
 {
 	var $wpa_packet_table = $('#wpa-packet-table');
 	var rowcount = $wpa_packet_table.find('tr').length;
+	var $clone = $wpa_packet_table.find('tr').eq(1).clone();
 
-	$wpa_packet_table.append('<tr><td>' + rowcount + '</td><td><textarea class = "wpa-packet-textarea"></textarea></td>');
+	$clone.find('td:first-child').text(rowcount);
+	$wpa_packet_table.append($clone);
+});
+
+$(document).on('click', '#wpa-packet-table td:first-child', function()
+{
+	var $this = $(this);
+	$this.parent('tr').remove();
+
+	var i = 1;
+
+	$('#wpa-packet-table tr').each(function()
+	{
+		if($(this).children('td').length > 0)
+		{
+			$(this).children('td').eq(0).text(i);
+			i++;
+		}
+	});
+});
+
+$(document).on('mouseenter', '#wpa-packet-table td:first-child', function()
+{
+	var $this = $(this);
+
+	$this.data('num', $this.text());
+	$this.text('X');
+});
+
+$(document).on('mouseleave', '#wpa-packet-table td:first-child', function()
+{
+	var $this = $(this);
+
+	$this.text($this.data('num'));
 });
