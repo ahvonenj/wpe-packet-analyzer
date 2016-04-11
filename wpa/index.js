1733d429859ad73add8edd266db03c7832989e16
Jonah <jonah.ahvonen@gmail.com>
2016-04-11 17:38:02 +0300

    Add help button (2)

--- wpa/index.js
+++ wpa/index.js
@@ -30,6 +30,14 @@ $(document).on('click', '#deletepackets', function()
 	}
 });
 
+$(document).on('click', '#help', function()
+{
+	alert('- Hold C to copy a group of bytes (which are hovered over with mouse) to clipboard\n' + 
+		'- Hold Z and click on bytes to mark them as important\n' +
+		'- Hold X and click obytes to mark them as not important\n' +
+		'- Hold A while hovering over a byte to see its decimal form')
+});
+
 $(document).on('click', '#loadpackets', function()
 {
 	$('.wpa-hider').fadeIn();
